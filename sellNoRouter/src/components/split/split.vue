9a66fa47963144479811980c96a5c3c24bfbdb4c
camizeng <xiaozeng.022@163.com>
2018-04-11 20:47:27 +0800

    new components

---
+++ sellNoRouter/src/components/split/split.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: "split"
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
